bf694b7822d58b5a1c0f2de4b5b6af16c23d9630
Deena Smith <deenasmith1630@gmail.com>
2022-03-08 12:09:40 -0600

    tasks trashed when dragged, also works on mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -236,3 +236,21 @@ $(".card .list-group").sortable({
 
 
 });
+
+
+
+// TRASH Items
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
